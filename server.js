7adc74dce937e2b6b9f2ded6273419d91d1ff84c
jacop3 <163762445+jacop3@users.noreply.github.com>
2024-03-24 15:47:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io');
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
