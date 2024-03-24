44d7194bb810d89c70219f5aad5196e53bfd1c05
jacop3 <163762445+jacop3@users.noreply.github.com>
2024-03-24 15:15:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ io.on('connection', (socket) => {
 
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
